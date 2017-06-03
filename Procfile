481aef93402bb93a4584ac6836ffb185c3fc16e6
Hayashi Hiroki <gyouzakuuka@gmail.com>
2017-06-03 15:55:18 +0000

    for heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn mysite_talkapp.wsgi
+web: gunicorn --env DJANGO_SETTINGS_MODULE=mysite_talkapp.settings mysite_talkapp.wsgi --log-file -
--- mysite_talkapp/settings.py
+++ mysite_talkapp/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'talkapp'
+    'talkapp',
+    'gunicorn'
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -141,12 +142,17 @@ USE_TZ = True
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-#STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = 'staticfiles'
 
-
+# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(PROJECT_ROOT, 'static'),
 )
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
